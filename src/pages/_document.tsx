668bb73f8cedc51450209a323ce310849a5f7914
Ana Howard <anahoward@Anas-MacBook-Air.local>
2022-12-31 20:49:37 +0100

    wip

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
 		<Html className="">
 			<Head>
 				<title>Ana Howard</title>
-				<link rel="icon" href="data:;base64,iVBORw0KGgo=" />
+				<link
+					rel="icon"
+					href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='.9em' font-size='90'%3E👀%3C/text%3E%3C/svg%3E"
+				/>
 				<meta name="description" content="👋 wagwan! welcome to my site" />
 			</Head>
 			<body className="bg-[#f7f2f2] dark:bg-gray-800">
