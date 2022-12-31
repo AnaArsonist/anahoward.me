88b71e1e709e815ad49b73a8b00be09f2c2b61b4
Ana Howard <anahoward@Anas-MacBook-Air.local>
2022-12-31 19:34:42 +0100

    init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
 	return (
 		<Html className="">
-			<Head />
+			<Head>
+				<title>AnaHoward.me</title>
+				<link rel="icon" href="data:;base64,iVBORw0KGgo=" />
+			</Head>
 			<body className="bg-[#f7f2f2] dark:bg-gray-800">
 				<Main />
 				<NextScript />
