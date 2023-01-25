d1f2927fb9d20724bb8463303e91d47a353afb6b
AnaArsonist <howardana6@gmail.com>
2023-01-25 01:40:20 +0000

    wip

--- meow/src/lib/consts.ts
+++
@@ -1,1 +0,0 @@
-export const APP_NAME = 'dApp Starter'
--- meow/src/pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-	res.status(200).json({ name: 'hello, world!' })
-}
-
-export default handler
