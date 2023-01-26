3d0069b5c3d77f9834917bf08eb722230a8c8705
AnaArsonist <howardana6@gmail.com>
2023-01-26 20:40:59 +0000

    wip

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -5,7 +5,7 @@ const IntroCard = ({ section }) => {
 	return (
 		<motion.div
 			className="col-span-3 md:col-span-2 bg-white dark:bg-gray-900
-			 rounded-3xl text-gray-600 dark:text-gray-500 aspect-2/1 pb-4 md:pb-0"
+			 rounded-3xl text-gray-600 dark:text-gray-300 aspect-2/1 pb-4 md:pb-0"
 			whileHover="groupHover"
 			animate={{ opacity: ['all'].includes(section) ? 1 : 1 }}
 			variants={{
@@ -43,7 +43,7 @@ const IntroCard = ({ section }) => {
 					<div className="text-3xl md:text-4xl 2xl:text-4xl 2xl:mt-16 m-6 ml-10 md:m-8 font-nunito font-bold">Hi, I&apos;m Ana!</div>
 				</div>
 				<div className="flex flex-col ml-10 justify-center 2xl:mt-10 sm:-mt-6 sm:ml-8">
-					<div className="lg:text-xl xl:text-2xl 2xl:mr-20 font-nunito text-gray-500">
+					<div className="lg:text-xl xl:text-2xl 2xl:mr-20 font-nunito dark:text-gray-300">
 						{' '}
 						I&apos;m an 18 year-old student doing a BASc in Interdisciplinary Problems and Methods @
 						<a
@@ -55,7 +55,7 @@ const IntroCard = ({ section }) => {
 							LIS
 						</a>
 					</div>
-					<div className="flex lg:text-xl xl:text-xl 2xl:text-2xl font-base text-gray-500 mt-5">
+					<div className="flex lg:text-xl xl:text-xl 2xl:text-2xl font-base dark:text-gray-300 mt-5">
 						I&apos;m interested in art, keyboards and meowies!
 					</div>
 				</div>
