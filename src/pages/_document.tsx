5c1f0d9c6afecc8370a07799110eadba2131c952
AnaArsonist <howardana6@gmail.com>
2023-02-02 23:37:22 +0000

    added analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
 	return (
@@ -22,14 +23,19 @@ export default function Document() {
 				<meta property="twitter:image" content="https://anahoward.me/images/preview.png"></meta>
 
 				{/* <!-- Google tag (gtag.js) --> */}
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-HERRG7L4LP"></script>
-				<script>
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
+				<Script
+       				 src="https://www.googletagmanager.com/gtag/js?id=G-HERRG7L4LP"
+        				strategy="afterInteractive"
+      			/>
+      			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){window.dataLayer.push(arguments);}
+				gtag('js', new Date());
 
-					gtag('config', 'G-HERRG7L4LP');
-				</script>
+				gtag('config', 'G-HERRG7L4LP');
+				`}
+			</Script>
 			</Head>
 			<body className="bg-[#f7f2f2] dark:bg-gray-800">
 				<Main />
